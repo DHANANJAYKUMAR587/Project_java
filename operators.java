7f033ea76c2648c3939b43c12748b25122b4baef
Dhananjay <your-dhananjayvats587@example.com>
2025-11-28 01:35:33 +0530

    Operators in java

--- operators.java
+++ operators.java
@@ -1,31 +1,31 @@
-class Operators {
+class operators {
     public static void main(String[] args) {
 
         int a = 10, b = 5;
 
         // Arithmetic Operators
-        System.out.println(a + b);  // +
-        System.out.println(a - b);  // -
-        System.out.println(a * b);  // *
-        System.out.println(a / b);  // /
-        System.out.println(a % b);  // %
+        System.out.println(a + b);  
+        System.out.println(a - b);  
+        System.out.println(a * b);  
+        System.out.println(a / b);  
+        System.out.println(a % b);  
 
         // Unary Operators
-        System.out.println(++a);    // pre-increment
-        System.out.println(b--);    // post-decrement
+        System.out.println(++a);   
+        System.out.println(b--);
 
         // Assignment Operators
-        a += 3;                     // a = a + 3
+        a += 3;                     
         System.out.println(a);
 
         // Relational Operators
-        System.out.println(a > b);  // >
-        System.out.println(a == b); // ==
+        System.out.println(a > b);  
+        System.out.println(a == b); 
 
         // Logical Operators
-        System.out.println(a > 5 && b < 10);  // &&
-        System.out.println(a > 5 || b > 10);  // ||
-        System.out.println(!(a > b));         // !
+        System.out.println(a > 5 && b < 10);  
+        System.out.println(a > 5 || b > 10);  
+        System.out.println(!(a > b));       
 
         // Ternary Operator
         int max = (a > b) ? a : b;
