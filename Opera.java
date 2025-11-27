a00b45a7f5a1944f63c1ed6da8cec7616e7c05e3
Dhananjay <your-dhananjayvats587@example.com>
2025-11-28 01:21:10 +0530

    second

---
+++ Opera.java
@@ -0,0 +1,14 @@
+public class Opera {
+    public static void main(String[] args) {
+
+        int a = 10, b = 5;
+
+        // Arithmetic Operators
+        System.out.println(a + b);  // +
+        System.out.println(a - b);  // -
+        System.out.println(a * b);  // *
+        System.out.println(a / b);  // /
+        System.out.println(a % b);  // % {
+    
+}
+}
