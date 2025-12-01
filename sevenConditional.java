45e4789c14fdb3673870f90e6b3876ffd9922a30
Dhananjay <your-dhananjayvats587@example.com>
2025-12-01 19:14:34 +0530

    Grade Marks

---
+++ sevenConditional.java
@@ -0,0 +1,27 @@
+public class sevenConditional {
+    public static void main(String[] args) {
+        
+    int marks =93;
+    if(marks >= 90){
+        System.out.println("Excellent");
+    }
+    else if (marks >= 80){
+        System.out.println("A+");
+    }
+    else if (marks >= 70){
+        System.out.println("A");
+    }
+    else if(marks >= 60){
+        System.out.println("B+");
+    }
+    else if(marks>= 50){
+        System.out.println("B");
+    }
+    else if(marks>= 40){
+        System.out.println("Pass");
+    }
+    else{
+        System.out.println("Fail");
+    }
+}
+}
