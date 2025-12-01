ddc86d2567c9161d6251420cfc0747da3d166996
Dhananjay <your-dhananjayvats587@example.com>
2025-12-01 19:27:45 +0530

    Checking leap year

---
+++ sixConditional.java
@@ -0,0 +1,11 @@
+public class sixConditional {
+    public static void main(String[] args) {
+        int year= 2024;
+        if((year % 4==0 && 100 !=0) || year % 400 ==0){
+            System.out.println("Leap year");
+        }
+        else{
+            System.out.println("Not a leap year");
+        }
+    }
+}
