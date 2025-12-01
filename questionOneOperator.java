7cc9048fd9ac7e297eb41890fd8b6d7b901075e3
Dhananjay <your-dhananjayvats587@example.com>
2025-12-01 17:41:19 +0530

    Opeators question one and two

---
+++ questionOneOperator.java
@@ -0,0 +1,8 @@
+public class questionOneOperator {
+    public static void main(String[] args) {
+        int a=22;
+        int b=4;
+        int result= ((a/b)+(a%b));
+        System.out.println(result);
+    }
+}
---
+++ questionTwoOperator.java
@@ -0,0 +1,7 @@
+public class questionTwoOperator {
+    public static void main(String[] args) {
+        int x=5;
+        x+=20;
+        System.out.println(x);
+    }
+}
