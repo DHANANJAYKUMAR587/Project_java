2768a9a7a91a468ac6558b674589e5fe2b63c9b8
Dhananjay <your-dhananjayvats587@example.com>
2025-12-01 19:38:05 +0530

    Sum of first 50 natural numbers

---
+++ secondLoops.java
@@ -0,0 +1,11 @@
+public class secondLoops {
+    public static void main(String[] args) {
+        int sum=0; 
+        int i=1;
+        while(i <= 50){
+            sum+=i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
