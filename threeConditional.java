a1c786caa422f85861a8de9fba5ae38d7368a744
Dhananjay <your-dhananjayvats587@example.com>
2025-12-01 18:54:19 +0530

    Even Or Odd

---
+++ threeConditional.java
@@ -0,0 +1,11 @@
+public class threeConditional {
+    public static void main(String[] args) {
+        int v=3;
+        if(v %2 == 0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
