16449d546faa5ba11eebc676a078690750caef7e
Dhananjay <your-dhananjayvats587@example.com>
2025-12-01 19:20:40 +0530

    Checking voting eligiblity

---
+++ nineConditional.java
@@ -0,0 +1,12 @@
+public class nineConditional {
+    public static void main(String[] args) {
+        int age=13;
+
+        if(age>= 18){
+            System.out.println("Eligible to vote");
+        }
+        else{
+            System.out.println("Not eligible");
+        }
+    }
+}
