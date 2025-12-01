e5a288583aebfc98111e7bfbd51ab42ea9d97ca2
Dhananjay <your-dhananjayvats587@example.com>
2025-12-01 18:57:12 +0530

    Largest of three numbers

---
+++ fourConditonal.java
@@ -0,0 +1,16 @@
+public class fourConditonal {
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        int c=3;
+        if(a>b && a>c){
+            System.out.println(a+" is largest");
+        }
+        else if(b>c){
+            System.out.println(b+" is largest");
+        }
+        else{
+            System.out.println(c+" is largest");
+        }
+    }
+}
